0a6882a43c07ba7eec467008936e3a284f187545
Debora Passos <deborinhapassos@gmail.com>
2021-08-10 16:34:39 -0300

    Desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,7 @@
+db.voos.findOne(
+  { 
+  "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] }, 
+  "aeroportoOrigem.sigla": "SBGR",
+  "aeroportoDestino.sigla": "KJFK" },
+  { _id: 0, vooId: 1 },
+  );
