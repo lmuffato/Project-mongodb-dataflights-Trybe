a4b59f9f334f92ecc5ec1831d06172ea30747f8d
RenzoSev <sevilharenzo@gmail.com>
2021-08-11 11:53:19 -0300

    feat: desafio24

---
+++ challenges/desafio24.js
@@ -0,0 +1,9 @@
+db.voos.find({ 
+  litrosCombustivel: { $exists: true, $lt: 600 },
+  "empresa:nome": { $nin: ["GOL", "AZUL"] } },
+  { 
+    vooId: 1,
+    "empresa.nome": 1, 
+    litrosCombustivel: 1,
+    _id: 0,
+  }).limit(1);
