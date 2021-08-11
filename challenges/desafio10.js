55df251545fd28858bf27e62730489932d12764b
Gui-lira <gui.lira.martins@gmail.com>
2021-08-11 13:44:59 -0300

    10 e 9

---
+++ challenges/desafio10.js
@@ -0,0 +1,9 @@
+db.voos.find({ ano: 2017, "empresa.nome": "GOL" }, {
+    _id: 0,
+    vooId: 1,
+    "empresa.nome": 1,
+    "aeroportoOrigem.nome": 1,
+    "aeroportoDestino.nome": 1,
+    ano: 1,
+    mes: 1,
+}).limit(10);
