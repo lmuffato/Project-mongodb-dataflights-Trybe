3e14edf40a0bdac3f56e958d0720fdfa4359ad7e
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-11 16:43:26 -0300

    Req 22 - Retorna vooId do primeiro voo que atende filtragem

---
+++ challenges/desafio22.js
@@ -0,0 +1,10 @@
+db.voos.findOne(
+  {
+    $and: [
+      { $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }] },
+      { "aeroportoOrigem.sigla": "SBGR" },
+      { "aeroportoDestino.sigla": "KJFK" },
+    ],
+  },
+  { vooId: 1, _id: 0 },
+);
