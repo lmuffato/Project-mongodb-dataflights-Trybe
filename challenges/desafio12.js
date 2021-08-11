f6df7121add4e8a0eab1c59d1b0ea76e49c280f7
Alexandre Oliveira <alexandremolive@gmail.com>
2021-08-11 16:07:43 -0300

    feat req12

---
+++ challenges/desafio12.js
@@ -0,0 +1,9 @@
+db.voos.count(
+  { $or: [ 
+    { "aeroportoDestino.pais": "BRASIL" },
+    
+    { "aeroportoDestino.pais": "ARGENTINA" },
+   
+    { "aeroportoDestino.pais": "CHILE" },
+  ] },
+);
