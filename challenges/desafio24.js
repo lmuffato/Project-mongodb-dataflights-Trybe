040512f399a3c3452f08dc480ab461bfd7a687de
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-08-11 16:11:29 -0300

    req 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,12 @@
+db.voos.find(
+  {
+    litrosCombustivel : { $lte: 600, $exists: true },
+    "empresa.nome": { $nin: ["GOL", "AZUL"] }
+  },
+  {
+    vooId: 1,
+    "empresa.nome": 1,
+    litrosCombustivel: 1,
+    _id: 0,
+  },
+).limit(1).pretty();
