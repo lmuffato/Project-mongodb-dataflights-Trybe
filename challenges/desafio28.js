705e46ceaeb680f40d19088ef7e82ce33ff0501d
Felipe Muller <felipeavmuller@gmail.com>
2021-08-11 18:06:22 -0300

    Desafio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,15 @@
+const LATAM = "LATAM AIRLINES BRASIL";
+
+db.resumoVoos.insertOne({
+  empresa: LATAM,
+  totalVoosDomesticos: db.voos.find({ $and: [
+    { "empresa.nome": LATAM },
+    { natureza: "Doméstica" },
+  ] }).count(),
+});
+
+db.resumoVoos.findOne({ empresa: LATAM }, {
+  empresa: 1,
+  totalVoosDomesticos: 1,
+  _id: 0,
+});
