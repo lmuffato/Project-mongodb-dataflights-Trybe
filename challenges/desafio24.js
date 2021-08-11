aa55d6244349553d1ff603b2acc08b88245c791e
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-08-11 19:45:19 -0300

    feat: desafio 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,10 @@
+db.voos.find({
+  $and: [
+    { litrosCombustivel: { $lte: 600 } },
+    { litrosCombustivel: { $exists: true } },
+    { $nor: [
+      { "empresa.nome": "GOL" },
+      { "empresa.nome": "AZUL" },
+    ] },
+  ],
+}, { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }).limit(1);
