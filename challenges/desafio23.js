8010d77900e12d35270117837b89b6463be58ae8
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-11 19:12:46 -0300

    feat: agradecimentos no arquivo do  req23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,3 +1,5 @@
+// agradecimentos a Ana ventura e Marilia cegalla pelas dicas sobre o projeto  
+
 db.voos.findOne({ $and: [
   { litrosCombustivel: { $lt: 1000 } },
   { litrosCombustivel: { $exists: true } },
