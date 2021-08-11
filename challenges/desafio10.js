691e52398c0e122e354d69dfc4b4e24d6989e455
carlos25xrm <carlos25xrm@gmail.com>
2021-08-11 15:49:29 -0300

    fix(2): desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,2 +1,3 @@
 db.voos.find({ "empresa.nome": "GOL", ano: { $eq: 2017 } },
-    { vooId: 1, "empresa.nome": 1, "aeroportoOrigem.nome": 1, "aeroportoDestino.nome": 1, mes: 1, ano: 1 }).limit(10);
+    { vooId: 1, "empresa.nome": 1, "aeroportoOrigem.nome": 1, "aeroportoDestino.nome": 1, mes: 1, ano: 1 })
+    .limit(10);
