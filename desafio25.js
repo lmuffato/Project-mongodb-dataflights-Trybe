c4feac05199731e1cc45d7702dac8300144e56fb
luanlsr <luanfswd@gmail.com>
2021-08-11 02:31:12 -0300

    feat: :sparkles: req 25 implementation

--- desafio25.js
+++ desafio25.js
@@ -1,1 +1,12 @@
 // 25 - Remova todos os voos da empresa AZUL em que a quantidade de combustível seja menor do que 400. Informe a quantidade de documentos removidos.
+db.voos.deleteMany({
+  $and: [{
+    "empresa.nome": "AZUL",
+  }, {
+    litrosCombustivel: {
+      $lt: 400,
+    },
+  }],
+}, {
+  litrosCombustivel: 1,
+});
