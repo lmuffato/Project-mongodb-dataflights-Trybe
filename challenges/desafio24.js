876a3534a92b23fe33568fb593e3dbf96410a80e
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-08-11 16:29:50 -0300

    Challenge 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,4 @@
+db.voos.find({ $and: [{ litrosCombustivel: { $not: { $gt: 600 } } }, 
+    { "empresa.nome": { $nin: ["GOL", "AZUL"] } }, 
+    { litrosCombustivel: { $exists: true } }] }, 
+    { _id: false, vooId: true, "empresa.nome": true, litrosCombustivel: true }).limit(1);
