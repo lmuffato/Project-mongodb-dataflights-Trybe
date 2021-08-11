4111c07312f98deec9bd9e480b96369bc226631e
Jodiel Briesemeister <jodielll29@gmail.com>
2021-08-11 15:39:17 -0300

    feat: req27

---
+++ challenges/desafio27.js
@@ -0,0 +1,10 @@
+db.resumoVoos.insertOne({ 
+    empresa: "PASSAREDO", 
+    totalVoosDomesticos: db.voos.count({ $and: [
+      { "empresa.nome": "PASSAREDO" },
+      { natureza: "Doméstica" },
+    ],
+    }),
+});
+  
+db.resumoVoos.find({ empresa: "PASSAREDO" });
