2d16d43a5d093681709ebf521bb6a64527c6b25e
MarcioFujita <hanker78@gmail.com>
2021-08-11 17:13:41 -0300

    Requisitos 21 ao 25

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,4 @@
-db.voos.find({ "empresa.nome": "GOL", ano: 2017 }, {
+db.voos.find({ "empresa.nome": "GOL", ano: 2017 }, { 
   vooId: 1,
   "empresa.nome": 1,
   "aeroportoOrigem.nome": 1,
@@ -6,4 +6,4 @@ db.voos.find({ "empresa.nome": "GOL", ano: 2017 }, {
   mes: 1,
   ano: 1,
   _id: 0,
-}).limit(10);
+  }).limit(10);
---
+++ challenges/desafio23.js
@@ -0,0 +1,6 @@
+db.voos.findOne({
+  $and: [
+    { litrosCombustivel: { $lt: 1000 } },
+    { litrosCombustivel: { $exists: true } },
+  ] },
+{ vooId: 1, litrosCombustivel: 1, _id: 0 });
