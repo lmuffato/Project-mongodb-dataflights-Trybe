314a1c878415f1c40812867713635ea61cdf1eaa
MuriloBatista <mub4si@gmail.com>
2021-08-11 17:30:24 -0300

    desafio24

---
+++ challenges/desafio24.js
@@ -0,0 +1,6 @@
+db.voos.findOne(
+  {
+    litrosCombustivel: { $lt: 600, $exists: true },
+    "empresa.nome": { $nin: ["GOL", "AZUL"] } },
+  { vooId: 1, litrosCombustivel: 1, "empresa.nome": 1, _id: 0 },
+  );
