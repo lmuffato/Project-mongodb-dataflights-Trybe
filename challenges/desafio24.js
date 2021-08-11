d3596f4157d4d360b10a3ad1f9b30745087cdc63
defauth98 <neto.daniribeiro@gmail.com>
2021-08-11 17:43:53 -0300

    req 21

---
+++ challenges/desafio24.js
@@ -0,0 +1,7 @@
+db.voos.findOne(
+  { $and: [
+      { litrosCombustivel: { $lte: 600 } }, 
+      { "empresa.nome": { $nin: ["GOL, AZUL"] } },
+  ] },
+  { vooId: 1, litrosCombustivel: 1, "empresa.nome": 1, _id: 0 },
+); 
