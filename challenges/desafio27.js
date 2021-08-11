87c13bda1f3239d1251eaab29eaf8c373a60b6c8
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-11 16:48:12 -0300

    Feat: Desafio27

---
+++ challenges/desafio27.js
@@ -0,0 +1,6 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.count({ "empresa.nome": "PASSAREDO", natureza: "Doméstica" }),
+});
+
+db.resumoVoos.findOne({ empresa: "PASSAREDO" }, { _id: 0 });
