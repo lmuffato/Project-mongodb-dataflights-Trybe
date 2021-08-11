7afc78d3c5539fb1d45f7a52f894caeb5cc2cd03
Orlando Flores <orlando_flores@id.uff.br>
2021-08-11 15:51:48 -0300

    feat: desafio 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,8 @@
+db.voos.find({
+  $and: [
+    { litrosCombustivel: { $not: { $gt: 600 } } },
+    { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
+    { litrosCombustivel: { $exists: true } }],
+},
+{ vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 })
+.limit(1);
