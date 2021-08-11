7803d504e0d4efe04dd8ecdb7e13d39f9137bb8b
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-08-11 17:58:53 -0300

    desafio 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,7 @@
+db.voos.find(
+  { $and: [
+    { litrosCombustivel: { $exists: true } },
+    { litrosCombustivel: { $not: { $gt: 1000 } } },
+  ] },
+  { vooId: 1, litrosCombustivel: 1, _id: 0 },
+).limit(1);
