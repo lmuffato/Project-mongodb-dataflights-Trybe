833a4b7e2421ddfe9f53c6f797414b570c63b2ee
carlos25xrm <carlos25xrm@gmail.com>
2021-08-11 19:51:00 -0300

    fix: desafio 26

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,6 +1,6 @@
 db.voos.deleteMany(
     {
         "empresa.nome": "GOL",
-        "passageiros.pagos": { $in: [5, 6, 7, 8, 9, 10]}
+        "passageiros.pagos": { $in: [5, 6, 7, 8, 9, 10] },
     },
 );
