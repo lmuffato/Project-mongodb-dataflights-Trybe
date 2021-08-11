bf43fb5a3880635e517d4d1ed9d47396e1652330
lucas-paz <lucas.hpaz@outlook.com>
2021-08-11 11:55:42 -0300

    add reqs from 6 to 10 and fixes req 5

---
+++ challenges/desafio10.js
@@ -0,0 +1,12 @@
+db.voos.find(
+  { ano: 2017, "empresa.nome": "GOL" },
+  {
+    vooId: 1,
+    "empresa.nome": 1,
+    "aeroportoOrigem.nome": 1,
+    "aeroportoDestino.nome": 1,
+    mes: 1,
+    ano: 1,
+    _id: 0,
+  },
+).limit(10);
