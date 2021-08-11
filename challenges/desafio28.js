e0acb483cfb8104c8bcffdd466faec1c0bd3e0b7
MuriloBatista <mub4si@gmail.com>
2021-08-11 19:37:14 -0300

    desafio28

---
+++ challenges/desafio28.js
@@ -0,0 +1,12 @@
+const company = "LATAM AIRLINES BRASIL";
+
+const queryResults = db.voos.find({
+  "empresa.nome": company, natureza: "Doméstica",
+}).count();
+
+db.resumoVoos.insertOne({
+  empresa: company,
+  totalVoosDomesticos: queryResults,
+});
+
+db.resumoVoos.find({ empresa: company }, { _id: 0 });
