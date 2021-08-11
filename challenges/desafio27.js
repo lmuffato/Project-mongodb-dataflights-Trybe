6bffe49cd9f747cb842f11d598d3246a187690f5
Marcelo Maurício <mmauricio.design@gmail.com>
2021-08-11 17:18:15 -0300

    feat: get info for a document and insert in another one - req 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,11 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.find({
+    "empresa.nome": "PASSAREDO",
+    natureza: "Doméstica",
+  }).count(),
+});
+
+db.resumoVoos.find({
+  empresa: "PASSAREDO",
+}, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
