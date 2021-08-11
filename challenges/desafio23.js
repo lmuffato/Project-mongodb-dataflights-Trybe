a4c8a475a39585d5b4d7f4d042f5db2798840311
Elisa <elisafranca.carvalho@gmail.com>
2021-08-11 16:34:55 -0300

    feat: requirement 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,6 @@
+db.voos.find({ 
+  $and: [
+    { litrosCombustivel: { $not: { $gt: 1000 } } }, 
+    { litrosCombustivel: { $exists: true } },
+  ] }, 
+  { _id: false, vooId: true, litrosCombustivel: true }).limit(1);
