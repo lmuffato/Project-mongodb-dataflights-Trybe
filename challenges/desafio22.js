d8a6c1856a70275519975f9243bb0c7aad5e7d1c
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-08-11 11:50:33 -0300

    Feat: desafio22

---
+++ challenges/desafio22.js
@@ -0,0 +1,5 @@
+db.voos.find({ 
+  $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }],
+  "aeroportoOrigem.sigla": "SBGR",
+  "aeroportoDestino.sigla": "KJFK" },
+  { vooId: 1, _id: 0 }).limit(1);
