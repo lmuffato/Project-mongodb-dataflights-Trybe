5ae9d7d1c8c536e9fb121c64ae124bf7fc779d84
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-08-11 17:18:51 -0300

    Challenge 28: fix

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,4 +1,4 @@
 const nome = "LATAM AIRLINES BRASIL";
 db.voos.find({ "empresa.nome": nome, natureza: "Doméstica" }).count();
 db.resumeVoos.insertOne({ empresa: nome, totalVoosDomesticos: 20026 });
-db.resumeVoos.find({ empresa: nome}, { _id: false });
+db.resumeVoos.find({ empresa: nome }, { _id: false });
