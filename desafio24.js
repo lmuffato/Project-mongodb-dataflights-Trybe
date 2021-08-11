76400e17d41dc286f7ffab5a09c469e608642938
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-08-11 18:21:11 -0300

    [new] desafio 24

---
+++ desafio24.js
@@ -0,0 +1,6 @@
+db.voos.find(
+  { litrosCombustivel: { $lt: 600 },
+    "empresa.nome": { $nin: ["GOL", "AZUL"] },
+  },
+  { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 },
+  ).limit(1);
