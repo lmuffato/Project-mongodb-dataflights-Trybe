831ace7ad15e7ddd578237bf18d4c3c46437ecac
Gustavo Sousa <gustavohl98@gmail.com>
2021-08-11 18:10:54 -0300

    Req 28 - conta qtd de voos domesticos em nova COLLECTION

---
+++ challenges/desafio28.js
@@ -0,0 +1,6 @@
+db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: 20026 });
+
+db.resumoVoos.find(
+  { empresa: "LATAM AIRLINES BRASIL" },
+  { empresa: 1, totalVoosDomesticos: 1, _id: 0 },
+);
