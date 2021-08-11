ffcca111d430a46cb93b45f58966be34459e5392
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-11 04:30:40 +0000

    feat: desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,4 @@
+// 11 - Retorne a quantidade de documentos em que o campo
+// aeroportoDestino.pais não seja igual a ESTADOS UNIDOS.
+
+db.voos.count({ "aeroportoDestino.pais": { $not: { $eq: "ESTADOS UNIDOS" } } });
