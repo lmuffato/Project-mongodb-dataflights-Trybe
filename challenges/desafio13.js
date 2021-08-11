2ac4668a7e5de428d0b6c89a7f5da7a5d640a31c
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-08-11 15:42:10 -0300

    Challenge 13: fix

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,1 +1,1 @@
-db.voos.find({ "aeroportoDestino.continente": { $nin: ["EUROPA", "Ásia", "OCEANIA"] } }).count();
+db.voos.find({ "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } }).count();
