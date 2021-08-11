79c675dc72896e91d7cdf01355b4e5e024847e8b
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-08-11 16:45:02 -0300

    req 27 done

---
+++ challenges/desafio27.js
@@ -0,0 +1,11 @@
+let u = db.voos.find({
+    "empresa.nome": "PASSAREDO",
+    "natureza": "Doméstica",
+}).count();
+
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: u,
+});
+
+db.resumoVoos.find({}, { _id: 0 }).limit(1);
