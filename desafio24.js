2e5787f6471773508218b4c6d8f2c5ef0af5fb8b
luanlsr <luanfswd@gmail.com>
2021-08-11 02:26:24 -0300

    feat: req 24 implementation

--- desafio24.js
+++ desafio24.js
@@ -1,1 +1,16 @@
 // 24 - Retorne o vooId, empresa.nome e litrosCombustivel do primeiro voo em que litrosCombustivel não seja maior do que 600 e a empresa não seja GOL ou AZUL, e o campo litrosCombustivel exista.
+db.voos.find({
+  $and: [{
+    litrosCombustivel: { $not: { $gt: 600 } },
+  }, {
+    $or: [{ "empresa.nome": { $ne: "GOL" } }, { "empresa.nome": { $ne: "AZUL" } }],
+  },
+  {
+    litrosCombustivel: { $exists: true },
+  }],
+}, {
+  _id: 0,
+  vooId: 1,
+  "empresa.nome": 1,
+  litrosCombustivel: 1,
+}).limit(1);
