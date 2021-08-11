321d49cccca929ab93f9bd629d9dd831d2133c97
lucas-paz <lucas.hpaz@outlook.com>
2021-08-11 16:10:03 -0300

    fixes

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,1 +1,3 @@
-db.voos.find({ "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } }).count();
+db.voos.find(
+  { "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } },
+).count();
--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,1 +1,3 @@
-db.voos.find({ "aeroportoOrigem.pais": { $ne: "BRASIL" } }).count();
+db.voos.find(
+  { "aeroportoOrigem.pais": { $ne: "BRASIL" } },
+).count();
