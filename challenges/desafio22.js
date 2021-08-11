fba81ded076cc37f56a1e7b1a4b6b0f51dfeac03
Jodiel Briesemeister <jodielll29@gmail.com>
2021-08-11 15:15:05 -0300

    feat: req22

---
+++ challenges/desafio22.js
@@ -0,0 +1,6 @@
+db.voos.findOne({ $and: [
+    { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } },
+    { "aeroportoOrigem.sigla": "SBGR" },
+    { "aeroportoDestino.sigla": "KJFK" },
+  ],
+}, { _id: 0, vooId: 1 });
