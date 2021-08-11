3eba815c1bb1a0f34a0b9b8477963e785d7e17ee
MarcioFujita <hanker78@gmail.com>
2021-08-11 17:15:09 -0300

    Requisitos 21 ao 25 V2

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -2,6 +2,6 @@ db.voos.findOne({
   $and: [
     { litrosCombustivel: { $lt: 600 } },
     { litrosCombustivel: { $exists: true } },
-    { "empresa.nome": { $nin: [ "GOL", "AZUL" ] } },
+    { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
   ] },
 { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
