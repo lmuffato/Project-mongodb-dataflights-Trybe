657f21846108dd2d291cf8a36f5bb557973a4e47
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-08-11 16:47:41 -0300

    req 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,11 @@
+let u = db.voos.find({
+    "empresa.nome": "LATAM AIRLINES BRASIL",
+    "natureza": "Doméstica",
+}).count();
+
+db.resumoVoos.insertOne({
+  empresa: "LATAM AIRLINES BRASIL",
+  totalVoosDomesticos: u,
+});
+
+db.resumoVoos.find({}, { _id: 0 }).limit(1);
