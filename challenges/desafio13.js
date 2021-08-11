b27830b64fa7b281084ca80742f4f9b030938161
guilhermemd <guilherme.md@gmail.com>
2021-08-11 17:36:32 -0300

    feat: :sparkles: req 13 implementation

---
+++ challenges/desafio13.js
@@ -0,0 +1,12 @@
+// 13 - Retorne a quantidade de documentos em que o campo aeroportoDestino.continente não seja igual a EUROPA, ÁSIA e OCEANIA.
+db.voos.count({ 
+  $nor: [{ 
+    "aeroportoDestino.continente": "EUROPA", 
+  }, 
+  { 
+    "aeroportoDestino.continente": "ÁSIA",
+  }, 
+  { 
+    "aeroportoDestino.continente": "OCEANIA",
+  }],
+});
