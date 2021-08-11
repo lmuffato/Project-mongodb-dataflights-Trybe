075371b2d7175cf5b04c7ae111df23df09970f42
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-11 15:27:18 -0300

    Feat: desafio23

---
+++ challenges/desafio23.js
@@ -0,0 +1,7 @@
+db.voos.findOne({ 
+  $and: [{
+    litrosCombustivel: { $exists: true } }, 
+    { litrosCombustivel: { $lte: 1000 } }],
+  }, { 
+    _id: 0, vooId: 1, litrosCombustivel: 1,
+  });
