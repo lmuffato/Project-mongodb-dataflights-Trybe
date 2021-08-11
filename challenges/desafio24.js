3724893345cf91e4a1bbc0088ca6891d934b3667
carlos25xrm <carlos25xrm@gmail.com>
2021-08-11 19:29:06 -0300

    fix(2): desafio 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -4,6 +4,9 @@ db.voos.find(
         "empresa.nome": { $nin: ["GOL", "AZUL"] },
     },
     {
-        vooId: 1, "empresa.nome": 1,
-        litrosCombustivel: 1, _id: 0,
-    }).limit(1);
+        vooId: 1,
+        "empresa.nome": 1,
+        litrosCombustivel: 1,
+        _id: 0,
+    },
+).limit(1);
