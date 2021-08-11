b9df01e3a4568ea389c4b9608c99f6524515a3ae
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-08-11 15:23:48 -0300

    Challenge 10: fix

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -3,5 +3,6 @@ db.voos.find({ $and: [{ "empresa.nome": "GOL" }, { ano: 2017 }] },
     vooId: true,
     "empresa.nome": true,
     "aeroportoOrigem.nome": true,
+    "aeroportoDestino.nome": true,
      mes: true,
      ano: true }).limit(10);
