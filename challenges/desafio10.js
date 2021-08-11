e1f95cee5da676583c42dae6582d4158d2110724
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-08-11 15:41:05 -0300

    fix: slint challenge10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,7 +1,12 @@
 db.voos.find(
   { "empresa.nome": /^gol/gi, ano: 2017 },
   {
-    _id: false, vooId: true, "empresa.nome": true, "aeroportoOrigem.nome": true,
-    "aeroportoDestino.nome": true, ano: true, mes: true
-  }
+    _id: false,
+vooId: true,
+"empresa.nome": true,
+"aeroportoOrigem.nome": true,
+    "aeroportoDestino.nome": true,
+ano: true,
+mes: true,
+  },
 ).limit(10);
