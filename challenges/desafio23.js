32288251fffe12c72f9dfaccaf48066a6e4d56b6
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-08-11 17:03:13 -0300

    :sparkles: Requirement 23
    23 - Retorne o vooId e litrosCombustivel do primeiro voo em que o campo litrosCombustivel não seja maior do que 1000 e o campo litrosCombustivel exista.

---
+++ challenges/desafio23.js
@@ -0,0 +1,17 @@
+// 23 - Retorne o vooId e litrosCombustivel do primeiro voo em que o campo litrosCombustivel não seja maior do que 1000 e o campo litrosCombustivel exista.
+db.voos.find({
+  $and: [
+    {
+      litrosCombustivel: {
+        $exists: 1,
+      },
+    },
+    {
+      litrosCombustivel: {
+        $not: {
+          $gt: 1000,
+        },
+      },
+    },
+  ],
+}, { _id: 0, vooId: 1, litrosCombustivel: 1 }).limit(1);
