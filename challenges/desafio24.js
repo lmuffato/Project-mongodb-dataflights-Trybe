5f2283653e955c35f32c2a5a9da1bf10863efaa2
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-11 05:31:10 +0000

    feat: desafio 25

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -5,5 +5,5 @@ db.voos.findOne({
   $and: [
     { litrosCombustivel: { $lte: 600, $exists: true } },
     { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
-  ]
+  ],
 }, { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
