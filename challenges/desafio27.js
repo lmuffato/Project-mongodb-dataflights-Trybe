43262d84b532199e4ff269b4399a9216a5ee0bf2
phlopes <phlopes93@gmail.com>
2021-08-11 18:47:42 -0300

    fix: add the total flights with find() as per the flights document

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,6 +1,7 @@
 db.resumoVoos.insertOne({
 empresa: "PASSAREDO",
-totalVoosDomesticos: 4187
+totalVoosDomesticos: db.voos
+.find({ "empresa.nome": "PASSAREDO", natureza: "Doméstica" }).count(),
 });
 
 db.resumoVoos.find({}, {_id: 0 });
