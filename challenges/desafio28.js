df8827b1501170580ef7a9449f581de486a16fa8
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-08-11 15:20:56 -0300

    Feat: desafio28

---
+++ challenges/desafio28.js
@@ -0,0 +1,13 @@
+const LATAM = "LATAM AIRLINES BRASIL";
+
+db.resumoVoos.insertOne(
+    { 
+      empresa: LATAM,
+      totalVoosDomesticos: db.voos
+      .find({ "empresa.nome": LATAM, natureza: "Doméstica" }).count(),
+    },
+  );
+  
+  db.resumoVoos.find(
+    { empresa: LATAM }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 },
+  ).limit(1);
