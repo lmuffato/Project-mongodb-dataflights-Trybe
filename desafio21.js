eeb0c98a7bf72f99a22fabb8c81fb37561f15004
luanlsr <luanfswd@gmail.com>
2021-08-11 01:43:08 -0300

    feat: :sparkles: req 21 implementation

--- desafio21.js
+++ desafio21.js
@@ -1,1 +1,2 @@
 // 21 - Retorne o vooId do primeiro voo em que o campo litrosCombustivel seja maior ou igual a 1000.
+db.voos.find({ litrosCombustivel: { $gte: 1000 } }, { _id: 0, vooId: 1 }).limit(1);
