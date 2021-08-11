166c20ed047ca82b2ce9fd082a05adbe492c07db
Marcus C. H. Filho <mchusemann@gmail.com>
2021-08-11 15:26:05 -0300

    feat: desafio 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,6 @@
+db.voos.findOne(
+    { $and: [
+        { litrosCombustivel: { $not: { $gt: 1000 } } }, 
+        { litrosCombustivel: { $exists: true } }] },
+    { vooId: 1, litrosCombustivel: 1, _id: 0 },
+);
