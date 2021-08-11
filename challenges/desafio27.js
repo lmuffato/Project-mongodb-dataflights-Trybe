f6c97dab4088e86b29c65765c63683fb1e033a62
Bruno Mendes <mendessbruno@gmail.com>
2021-08-11 14:24:28 -0300

    resolvido desafio 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,7 @@
+db.resumoVoos.insertOne({ 
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.find({ $and: [{ "empresa.nome": "PASSAREDO" },
+  { natureza: "Doméstica" }] }).count(),
+});
+
+db.resumoVoos.find({}, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
