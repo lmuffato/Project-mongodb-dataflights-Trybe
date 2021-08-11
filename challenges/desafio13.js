981c42059ca446e184b7bd4fbd2fe86cdd35dc5b
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-08-11 04:53:58 +0000

    fix: eslint

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,4 +1,4 @@
 // 13 - Retorne a quantidade de documentos em que o campo
 // aeroportoDestino.continente não seja igual a EUROPA, ÁSIA e OCEANIA.
 
-db.voos.count({ "aeroportoDestino.continente": { $nin: [ "EUROPA", "ÁSIA", "OCEANIA" ] } });
+db.voos.count({ "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } });
--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,3 +1,3 @@
 // 20 - Retorne o vooId do primeiro voo em que o campo rtk não exista.
 
-db.voos.findOne({rtk: { $exists: false } }, {vooId: 1, _id: 0 });
+db.voos.findOne({ rtk: { $exists: false } }, { vooId: 1, _id: 0 });
