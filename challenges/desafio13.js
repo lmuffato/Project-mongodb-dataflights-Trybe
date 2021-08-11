6aaa4ae14fd2c9a38ced51de11aed305144559eb
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-11 15:45:20 -0300

    Desafio 13 Corrigido

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,5 +1,5 @@
 db.voos.count({ $and: [
-   { "aeroportoDestino.continente": {$ne:  "EUROPA" } }, 
-   { "aeroportoDestino.continente": {$ne: "ÁSIA" } },
-   { "aeroportoDestino.continente": {$ne: "OCEANIA" } }],
+   { "aeroportoDestino.continente": { $ne: "EUROPA" } }, 
+   { "aeroportoDestino.continente": { $ne: "ÁSIA" } },
+   { "aeroportoDestino.continente": { $ne: "OCEANIA" } }],
   });
