7e4c8f44a3b42a9198a3e7fbeadddefc51d2330e
w-cypriano <profwellington17@gmail.com>
2021-08-11 23:44:00 -0300

    Desafio21 e desafio22

---
+++ challenges/desafio22.js
@@ -0,0 +1,6 @@
+db.voos.findOne(
+    { $or: [{ "empresa.nome":"DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }], 
+    $and: [{ "aeroportoOrigem.sigla": "SBGR" }, { "aeroportoDestino.sigla": "KJFK" }] 
+    }, 
+    { vooId: 1, _id: 0 }
+);
