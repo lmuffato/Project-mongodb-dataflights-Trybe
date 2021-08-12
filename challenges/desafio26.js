6fb966768180b2276997e97e1539ad1fb706361f
lmuffato <lmiremuffato@gmail.com>
2021-08-12 13:38:43 -0300

    requisito 26

---
+++ challenges/desafio26.js
@@ -0,0 +1,6 @@
+/* 26 - Remova todos os voos da empresa GOL em que a quantidade de passageiros
+pagos esteja entre 5 e 10, incluindo os casos em que a quantidade é 5 e 10.
+Informe a quantidadede documentos removidos. */
+db.voos.deleteMany({
+ $and: [{ "empresa.nome": "GOL" }, { "passageiros.pagos": { $gte: 5, $lte: 10 } }],
+});
