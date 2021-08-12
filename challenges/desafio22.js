eb9a736db97bb04fd5a5c3aff006ad6c3db7b183
Luciano Amâncio <madruga665@hotmail.com>
2021-08-11 22:47:47 -0300

    desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,7 @@
+db.voos.find({ 
+  $and: [
+    { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } }, 
+    { "aeroportoOrigem.sigla": "SBGR" },
+    { "aeroportoDestino.sigla": "KJFK" },
+] },
+{ _id: false, vooId: true }).limit(1);
