2260e5a23b932103715f93940c118d9b64f0bdfe
Gabriel Ferreira <glucas709@gmail.com>
2021-08-11 21:44:44 -0300

    Challenge 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,10 @@
+db.voos.findOne(
+  {
+    $and: [
+      { litrosCombustivel: { $exists: true } },
+      { litrosCombustivel: { $not: { $gt: 600 } } },
+      { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
+    ],
+  },
+  { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
+);
