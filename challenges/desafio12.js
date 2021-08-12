9633a279eb0db3384a2fc406e1b9bb2855109cec
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-08-12 17:53:55 -0400

    feat: desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,2 @@
+// Retorne a quantidade de documentos em que o campo aeroportoDestino.pais seja igual a BRASIL, ARGENTINA ou CHILE
+db.voos.find({ "aeroportoDestino.pais": { $in: ["BRASIL", "ARGENTINA", "CHILE"] } }).count();
