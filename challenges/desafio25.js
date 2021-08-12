f48284bb72f7e977e030fca705b75b49f0cd6a9f
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-08-12 19:41:56 -0400

    feat: desafio 25

---
+++ challenges/desafio25.js
@@ -0,0 +1,4 @@
+// Remova todos os voos da empresa AZUL em que a quantidade de combustível seja menor do que 400. Informe a quantidade de documentos removidos
+db.voos.deleteMany({ $and: [{ "empresa.nome": "AZUL" },
+  { litrosCombustivel: { $exists: true } },
+  { litrosCombustivel: { $lt: 400 } }] });
