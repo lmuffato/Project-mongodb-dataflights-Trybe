0b37c90170fd9a78b9680789dd3ee09c7ac074cd
carlos25xrm <carlos25xrm@gmail.com>
2021-08-12 11:07:12 -0300

    feat: desafio 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,9 @@
+db.resumoVoos.insert({
+    empresa: "PASSAREDO",
+    totalVoosDomesticos: db.voos.find(
+        {
+          "empresa.nome": "PASSAREDO",
+          natureza: "Doméstica",
+        }).count() });
+
+db.resumoVoos.find({}, { empresa: 1, totalVoosDomesticos: 1, _id: 0 }).limit(1);
