ea6e42b96238ee4e688211432d06745ddab6587b
w-cypriano <profwellington17@gmail.com>
2021-08-11 21:39:32 -0300

    fix2 Desafio10, desafio9

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -3,7 +3,10 @@ db.voos.find(
     { vooId: 1, 
         "empresa.nome": 1,
         "aeroportoOrigem.nome": 1,
-        "aeroportoDestino.nome": 1, mes: 1, ano: 1, _id: 0
-    }
+        "aeroportoDestino.nome": 1,
+        mes: 1,
+        ano: 1,
+        _id: 0,
+    },
 )
 .limit(10);
