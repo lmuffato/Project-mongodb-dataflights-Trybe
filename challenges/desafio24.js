45e41352934856892c8e8c3ab57b81badc99e1a4
w-cypriano <profwellington17@gmail.com>
2021-08-12 00:37:37 -0300

    fix Desafio23 e desafio24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,5 +1,5 @@
 db.voos.findOne(
     { litrosCombustivel: { $exists: true, $lte: 600 }, 
     $nor: [{ "empresa.nome": "GOL" }, { "empresa.nome": "AZUL" }] }, 
-    { vooId: 1, "empresa.nome": 1,  litrosCombustivel: 1, _id: 0 },
+    { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 },
 );
