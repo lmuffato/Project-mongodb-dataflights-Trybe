4913dad5302b5e5196040f0ce433c5aabc50327d
Southier <patrick.southier@hotmail.com>
2021-08-12 00:12:03 -0300

    feat: completes requirements 23 and 24

---
+++ challenges/desafio23.js
@@ -0,0 +1,4 @@
+db.voos.find({
+  litrosCombustivel: { $exists: true, $not: { $gt: 1000 } } },
+  { _id: 0, vooId: 1, litrosCombustivel: 1 })
+    .limit(1);
---
+++ challenges/desafio24.js
@@ -0,0 +1,5 @@
+db.voos.find({
+  litrosCombustivel: { $exists: true, $not: { $gt: 600 } },
+  $or: [{ "empresa.nome": { $ne: "GOL" } }, { "empresa.nome": { $ne: "AZUL" } }],
+},
+{ _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }).limit(1);
