ce40501353305abf16e91f47aef50a94d369f620
Tiago Santos <tiago881@gmail.com>
2021-08-11 22:39:05 -0300

    requirement:22

---
+++ challenges/desafio22.js
@@ -0,0 +1,8 @@
+db.voos.findOne(
+  {
+    "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+    "aeroportoOrigem.sigla": "SBGR",
+    "aeroportoDestino.sigla": "KJFK",
+  },
+  { _id: 0, vooId: 1 },
+);
