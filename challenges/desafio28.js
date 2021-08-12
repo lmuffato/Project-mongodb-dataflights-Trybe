4b1632de448a560fb5ad89293a3cb7ea08cb1397
Nathalia Zebral <natzebral@gmail.com>
2021-08-12 11:19:40 -0300

    desafio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,12 @@
+db.resumoVoos.insert(
+    { empresa: "LATAM AIRLINES BRASIL",
+    totalVoosDomesticos: db.voos.find({
+        "empresa.nome": "LATAM AIRLINES BRASIL", 
+        natureza: "Doméstica",
+    }).count(),
+},
+);
+
+db.resumoVoos.find({ empresa: "LATAM AIRLINES BRASIL" },
+    { empresa: 1, totalVoosDomesticos: 1, _id: 0,
+    }).limit(1);
