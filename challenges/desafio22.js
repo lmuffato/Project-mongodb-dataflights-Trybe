c76e57e143c0e94202b9421a038b59dfcc5fabcc
Southier <patrick.southier@hotmail.com>
2021-08-12 00:11:43 -0300

    feat: completes requirements 21 and 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,9 @@
+db.voos.find({ $and: [
+  {
+    "aeroportoOrigem.sigla": { $eq: "SBGR" },
+    "aeroportoDestino.sigla": { $eq: "KJFK" },
+  },
+  { $or: [
+    { "empresa.nome": { $eq: "DELTA AIRLINES" } },
+    { "empresa.nome": { $eq: "AMERICAN AIRLINES" } },
+] }] }, { _id: 0, vooId: 1 }).limit(1);
