cbbcf08eab032ed0dc9b9570e80958388c035164
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-11 21:59:26 -0300

    Desafio 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,7 @@
+db.voos.find(
+    { $and: [
+        { litrosCombustivel: { $ne: { $gt: 1000 } } },
+        { litrosCombustivel: { $exists: true } },
+    ] },
+    { vooId: 1, litrosCombustivel: 1, _id: 0 },
+).limit(1);
