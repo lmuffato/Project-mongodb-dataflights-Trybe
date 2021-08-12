c9afb34ad4b6006956744a8af44705f53cef5613
Nathalia Zebral <natzebral@gmail.com>
2021-08-12 11:07:39 -0300

    desafio 27 - correcao lint

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,3 +1,12 @@
-db.resumoVoos.insert({ empresa: "PASSAREDO", totalVoosDomesticos: db.voos.find({ "empresa.nome": "PASSAREDO", natureza: "Doméstica" }).count() });
+db.resumoVoos.insert(
+    { empresa: "PASSAREDO",
+    totalVoosDomesticos: db.voos.find({
+        "empresa.nome": "PASSAREDO", 
+        natureza: "Doméstica",
+    }).count(),
+},
+);
 
-db.resumoVoos.find({}, { empresa: 1, totalVoosDomesticos: 1, _id: 0 }).limit(1);
+db.resumoVoos.find({},
+    { empresa: 1, totalVoosDomesticos: 1, _id: 0,
+    }).limit(1);
