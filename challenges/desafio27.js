7faf7f6241e24dc7d7e6385a7c70c27c7fc039bd
mallmann02 <leomallmann020302@gmail.com>
2021-08-12 22:39:38 -0300

    Challenges: c 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,7 @@
+db.resumoVoos
+  .insertOne({ empresa: "PASSAREDO", totalVoosDomesticos: 4187 });
+db.resumoVoos
+  .find(
+    { empresa: "PASSAREDO" },
+    { empresa: 1, totalVoosDomesticos: 1, _id: 0 },
+  ).limit(1);
