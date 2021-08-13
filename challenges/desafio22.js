a097bf185c3d1dbc9fba35c47a5c85898fd912e5
mallmann02 <leomallmann020302@gmail.com>
2021-08-12 22:39:38 -0300

    Challenges: c 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,15 @@
+db.voos
+  .find(
+    {
+      $and: [
+        { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } },
+        { "aeroportoOrigem.sigla": "SBGR" },
+        { "aeroportoDestino.sigla": "KJFK" },
+      ],
+    },
+    {
+      vooId: 1,
+      _id: 0,
+    },
+)
+    .limit(1);
