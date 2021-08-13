86dc17038fd068c450801c6c4ab108f869236b1f
Gabriel <g.pereira.a.m@gmail.com>
2021-08-12 21:51:50 -0300

    feat(req 28) - :sparkle: Desafio 28 feito

---
+++ challenges/desafio28.js
@@ -0,0 +1,24 @@
+const empresa = "LATAM AIRLINES BRASIL";
+
+db.resumoVoos.insertOne(
+  {
+    empresa,
+    totalVoosDomesticos: db.voos.count(
+      {
+        "empresa.nome": empresa,
+        natureza: "Doméstica",
+      },
+    ),
+  },
+);
+
+db.resumoVoos.findOne(
+  {
+    empresa,
+  },
+  {
+    _id: 0,
+    empresa: 1,
+    totalVoosDomesticos: 1,
+  },
+);
