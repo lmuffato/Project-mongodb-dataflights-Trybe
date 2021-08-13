4a652b5a8113288ba68f3a8488e061015c73ca6a
Gabriel <g.pereira.a.m@gmail.com>
2021-08-12 21:49:32 -0300

    feat(req 24) - :sparkle: Desafio 24 feito

---
+++ challenges/desafio24.js
@@ -0,0 +1,21 @@
+db.voos.findOne(
+  {
+    $and: [
+      {
+        litrosCombustivel: { $lt: 600 },
+      },
+      {
+        "empresa.nome": { $nin: ["GOL", "AZUL"] },
+      },
+      {
+        litrosCombustivel: { $exists: 1 },
+      },
+    ],
+  },
+  {
+    _id: 0,
+    vooId: 1,
+    "empresa.nome": 1,
+    litrosCombustivel: 1,
+  },
+);
