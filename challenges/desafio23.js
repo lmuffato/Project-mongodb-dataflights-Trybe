c4641669fffafdb8835c12508ee822816a2b31d1
Perycles reis <perycles_reis@hotmail.com>
2021-08-13 15:08:07 -0300

    desafio 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,5 @@
+// 23 - Retorne o vooId e litrosCombustivel do primeiro voo em que o campo litrosCombustivel não seja maior do que 1000 e o campo litrosCombustivel exista.
+
+db.voos
+.find({ litrosCombustivel: { $exists: 1, $lt: 1000 } }, { _id: 0, vooId: 1, litrosCombustivel: 1 })
+  .limit(1);
