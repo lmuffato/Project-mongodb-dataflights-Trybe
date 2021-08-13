247a8c85d88a15add187753e46af6ce753821579
Joao Nasc <oi@joaonasc.dev>
2021-08-13 18:50:36 -0300

    feat: challenge 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,9 @@
+const empresa = "LATAM AIRLINES BRASIL";
+
+const totalVoosDomesticos = db.voos.count(
+  { "empresa.nome": empresa, natureza: "Doméstica" },
+);
+
+db.resumoVoos.insertOne({ empresa, totalVoosDomesticos });
+
+db.resumoVoos.findOne({ empresa }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
