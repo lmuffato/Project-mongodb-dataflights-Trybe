d89ed0caba94bd39bfe39fec31b99ea4846088c9
Robertson <robertson.nunes@gmail.com>
2021-08-14 16:54:28 -0300

    Requisito 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,7 @@
+// 22 - Retorne o vooId do primeiro voo em que a empresa seja DELTA AIRLINES ou AMERICAN AIRLINES, a sigla do aeroporto de origem seja SBGR e a sigla do aeroporto de destino seja KJFK.
+db.voos.find(
+  { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+  "aeroportoOrigem.sigla": "SBGR",
+  "aeroportoDestino.sigla": "KJFK" },
+  { _id: 0, vooId: 1 },
+  ).limit(1);
