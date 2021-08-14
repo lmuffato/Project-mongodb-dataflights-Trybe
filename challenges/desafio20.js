546e322b87df2acea7446e99072eca31753e909a
igson <igson660@gmail.com>
2021-08-14 18:15:13 -0500

    desafio 22

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,1 +1,3 @@
-db.voos.findOne({ rtk: { $exists: false } }, { vooId: 1, _id: 0 });
+db.voos.findOne({ "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+  "aeroportoOrigem.sigla": "SBGR", 
+  "aeroportoDestino.sigla": "KJFK" }, { _id: 0, vooId: 1 });
