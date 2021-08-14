d4aa11b85a5e52413f4bb3f9d32331a27b2cf36e
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-08-14 16:20:00 -0300

    Feat: (req-22) Return the vooId of the first flight where the company is DELTA AIRLINES or AMERICAN AIRLINES, the abbreviation of the airport of origin is SBGR and the abbreviation of the airport of destination is KJFK.

---
+++ challenges/desafio22.js
@@ -0,0 +1,11 @@
+// use("dataFlights");
+// db.voos.find();
+
+db.voos.findOne(
+  {
+    "empresa.nome": { $in: ["DELTA", "AIRLINES", "AMERICAN AIRLINES"] },
+    "aeroportoOrigem.sigla": "SBGR",
+    "aeroportoDestino.sigla": "KJFK",
+  },
+  { _id: 0, vooId: 1 },
+);
