91e9157e127fd310fd1b0b2e55f979201dcc1d8f
andy silva <andssilva.23@gmail.com>
2021-08-15 07:13:02 -0400

    feat(req 22): :rocket: challenge 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,9 @@
+// use("dataFlights")
+db.voos.findOne(
+  { $and: [
+    { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } },
+    { "aeroportoOrigem.sigla": "SBGR" },
+    { "aeroportoDestino.sigla": "KJFK" },
+  ] },
+  { _id: 0, vooId: 1 },
+);
