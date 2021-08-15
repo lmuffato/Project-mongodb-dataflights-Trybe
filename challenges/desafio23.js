dee74c4396d5ddb859db1f03dd7356b9f0cb23b5
biazidioti <bzferreira.bio@gmail.com>
2021-08-15 10:20:32 -0300

    Challenge 23 e 24

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,6 @@
 /* 23 - Retorne o vooId e litrosCombustivel do primeiro voo em que o campo litrosCombustivel não seja maior do que 1000 e o campo litrosCombustivel exista. */
+db.voos.findOne({
+  $and: [
+    { litrosCombustivel: { $lte: 1000 } },
+    { litrosCombustivel: { $exists: true } }],
+}, { vooId: 1, litrosCombustivel: 1, _id: 0 });
--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,1 +1,8 @@
 /* 24 - Retorne o vooId, empresa.nome e litrosCombustivel do primeiro voo em que litrosCombustivel não seja maior do que 600 e a empresa não seja GOL ou AZUL, e o campo litrosCombustivel exista. */
+db.voos.findOne({
+  $and: [
+    { litrosCombustivel: { $lte: 600 } },
+    { litrosCombustivel: { $exists: true } },
+    { "empresa.nome": { $nin: ["GOL", "AZUL"] } }, 
+  ] },
+    { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
