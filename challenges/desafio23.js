c24454c177a34b3ebbbfb2064f6a1ff93b1e7dc9
andy silva <andssilva.23@gmail.com>
2021-08-15 07:18:34 -0400

    feat(req 23): :rocket: challenge 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,8 @@
+// use("dataFlights")
+db.voos.findOne(
+  { $and: [
+    { litrosCombustivel: { $not: { $gt: 1000 } } },
+    { litrosCombustivel: { $exists: true } },
+  ] },
+  { _id: 0, vooId: 1, litrosCombustivel: 1 },
+);
