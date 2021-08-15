21f0a16cdc1a6a9d99d49583daba71f1ac0b1da5
biazidioti <bzferreira.bio@gmail.com>
2021-08-15 10:19:45 -0300

    Challenge 22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,8 @@
 /* 22 - Retorne o vooId do primeiro voo em que a empresa seja DELTA AIRLINES ou AMERICAN AIRLINES, a sigla do aeroporto de origem seja SBGR e a sigla do aeroporto de destino seja KJFK. */
+db.voos.findOne(
+  {
+  "empresa.nome": { $in: ["AIRLINES", "AMERICAN AIRLINES"] },
+  "aeroportoOrigem.sigla": "SBGR",
+  "aeroportoDestino.sigla": "KJFK" },
+  { vooId: 1, _id: 0 },
+  );
