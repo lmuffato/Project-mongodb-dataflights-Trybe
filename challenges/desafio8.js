988e357f70c983115d62037fc234b2696f2f334f
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-08-16 13:47:13 -0300

    requirement 14 completed

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,1 +1,1 @@
-db.voos.find({ ano: { $gt: 2016 } }.count());
+db.voos.find({ ano: { $gt: 2016 } }).count();
--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,1 +1,1 @@
-db.voos.find({ ano: { $gte: 2017, $lte: 2018 } }.count());
+db.voos.find({ ano: { $gte: 2017, $lte: 2018 } }).count();
