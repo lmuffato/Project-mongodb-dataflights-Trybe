2144ccd68457336230b5604ee4df47393f837f91
Willian Alves <willian.alvesb65@gmail.com>
2021-08-16 03:02:29 -0300

    Concluindo req 10

--- challenges/desfio10.js
+++
@@ -1,5 +0,0 @@
-// 10 - Retorne apenas os 10 primeiros documentos com voos da empresa GOL
-// do ano de 2017. Exiba apenas os campos vooId, empresa.nome, aeroportoOrigem.nome,
-// aeroportoDestino.nome, mes e ano.
-// use("dataFlights")
-db.voos.find({}).limit(10);
