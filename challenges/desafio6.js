73719d66662490902c909f3eb72ad21eb1104e2f
lucas-chave <lcchavesr150@gmail.com>
2021-08-16 16:44:31 -0300

    fix challenge 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -4,5 +4,5 @@ db.voos.find(
   { 
     vooId: 756807,
   },
-  { vooId: 1, "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1, _id: 0 },
+  { "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1, _id: 0 },
 );
