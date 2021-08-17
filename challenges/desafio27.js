8d47f1933feaf4f14ebdc6c1c2832116da23003f
ScriptCamilo <script.camilo@gmail.com>
2021-08-17 09:01:23 -0300

    fix: twenty-seventh requirement

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,16 +1,16 @@
-const EMPRESA = "PASSAREDO";
+const empresa = "PASSAREDO";
 
-const voosDomestico = db.voos.count({
+const totalVoosDomestico = db.voos.count({
   natureza: "Doméstica",
-  "empresa.nome": EMPRESA,
+  "empresa.nome": empresa,
 });
 
 db.resumoVoos.insertOne({
-  empresa: EMPRESA,
-  totalVoosDomestico: voosDomestico,
+  empresa,
+  totalVoosDomestico,
 });
 
-db.resumoVoos.findOne({}, {
+db.resumoVoos.findOne({ empresa }, {
   _id: 0,
   empresa: 1,
   totalVoosDomestico: 1,
@@ -23,3 +23,10 @@ db.resumoVoos.findOne({}, {
 // de variável, uma vez que foi pedido no requisito para criar
 // algumas queries diferentes e para isso preciso armazenar o valor
 // da query de contagem em algum lugar.
+
+// PS: Por algum motivo meu código não estava passando no github, 
+// então busquei soluções iguais a minha que passaram, 
+// achei uma idêntica em lógica ao que eu fiz, 
+// mas com uma implementação diferente. Estou enviando algo parecido
+// para ver se o github aceita!
+// https://github.com/tryber/sd-010-a-mongodb-dataflights/pull/96
