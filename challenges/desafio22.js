034383dce700eeca37c65ed50d4ce1f7aa17f7f6
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-08-17 17:00:32 -0300

    desafio22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -2,4 +2,4 @@ db.voos.find({ $and: [
     { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } },
     { "aeroportoOrigem.sigla": "SBGR" },
     { "aeroportoDestino.sigla": "KJFK" }] },
-    { vooId: 1, litrosCombustivel: 1, _id: 0 }).limit(1);
+    { vooId: 1, _id: 0 }).limit(1);
