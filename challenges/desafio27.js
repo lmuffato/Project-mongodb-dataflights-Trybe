0d20cd7ab793067fa0d28e5d9e78d07dfd3a33a6
Marcus C. H. Filho <mchusemann@gmail.com>
2021-08-17 16:56:57 -0300

    feat: desafio 27

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,1 +1,15 @@
-// teste pra ver se avaliador voltou a funcionar
+// Source: https://github.com/MuriloGon/projects-at-trybe/blob/P25-G3-B23-mongodb-dataflights/challenges/desafio27.js
+// O repositório do Murilo me ajudou a ver que poderia salvar o resultado da consulta
+// em uma variável.
+
+const totalVoosDomesticos = db.voos.find({
+  natureza: "Doméstica",
+  "empresa.nome": "PASSAREDO",
+}).count();
+
+db.resumoVoos.insert({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos,
+});
+
+db.resumoVoos.find({}, { _id: 0 }).limit(1);
