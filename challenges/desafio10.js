ff8e56e52a60d58b8ac4f67e35be0b908825aefc
Mateus Alencar <mtauws@gmail.com>
2021-08-17 12:58:15 -0300

    chore: comments p.2

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,3 +1,7 @@
+/**
+ * Consultei o repositório do Maycon Felipes para resolver essa parte.
+ * Link: https://github.com/tryber/sd-09-mongodb-dataflights/tree/mayconfelipe-mongodb-dataflights
+ */
 db.voos.find(
   { "empresa.nome": "GOL", ano: 2017 },
   { 
