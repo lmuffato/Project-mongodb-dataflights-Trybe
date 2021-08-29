87cd45ba52e69a0a28ed0982dc4642479b2db70e
Sabrina <sasa2605.alves@gmail.com>
2021-08-29 20:22:04 -0300

    First 9 requirements done

--- desafio10.js
+++ desafio10.js
@@ -1,5 +1,12 @@
-db.voos.find({ $and: [
-    { "empresa.nome": "GOL" },
-    { ano: 2017 },
-], {"vooId": 1, "empresa.nome": 1,  "aeroportoOrigem.nome": 1, "aeroportoDestino.nome": 1, "mes": 1, "ano":1},
-});
+// db.voos.find(
+//         { "empresa.nome": "GOL" },
+//         { ano: 2017 },
+//     {
+//         "vooId": 1, 
+//         "empresa.nome": 1,
+//         "aeroportoOrigem.nome": 1,
+//         "aeroportoDestino.nome": 1,
+//         "mes": 1,
+//         "ano": 1,
+//     }
+// );
