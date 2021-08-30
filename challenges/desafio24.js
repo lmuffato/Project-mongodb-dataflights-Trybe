7cfda329afd7b828b3e4db2101cd5fd988caf6e7
Renan Costa <renancosta21@gmail.com>
2021-08-29 21:32:15 -0300

    requisito 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,6 @@
+db.voos.find({
+  litrosCombustivel: { $exists: true, $lte: 600 },
+  "empresa.nome": { $nin: ["GOL", "AZUL"] },
+}, {
+  vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0,
+}).limit(1);
