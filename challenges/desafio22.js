969c2bd69780fe135d4e2269b8a3660880cdbf0a
Marcos Albuquerque <mpdsa20@gmail.com>
2021-08-31 19:57:10 -0300

    Desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,20 @@
+db.voos.find(
+  {
+    $or: [
+      {
+        "empresa.nome": "DELTA AIRLINES",
+        "aeroportoOrigem.sigla": "SBGR",
+        "aeroportoDestino.sigla": "KJFK",
+      },
+      {
+        "empresa.nome": "AMERICAN AIRLINES",
+        "aeroportoOrigem.sigla": "SBGR",
+        "aeroportoDestino.sigla": "KJFK",
+      },
+    ],
+  },
+  {
+    vooId: 1,
+    _id: 0,
+  },
+).limit(1);
