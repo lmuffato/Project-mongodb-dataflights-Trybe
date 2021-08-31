488fcc029206359b7d7983c98e50d1195184924f
jlfagundes <jlfagundes@gmail.com>
2021-08-31 17:07:54 -0300

    feat: challenge 24 done using findOne(), $lt, $exists and $nin[]

---
+++ challenges/desafio24.js
@@ -0,0 +1,20 @@
+db.voos.findOne(
+  {
+    litrosCombustivel: {
+      $lt: 600,
+      $exists: true,
+    },
+    "empresa.nome": {
+      $nin: [
+        "GOL",
+        "AZUL",
+      ],
+    },
+  },
+  {
+    vooId: 1,
+    "empresa.nome": 1,
+    litrosCombustivel: 1,
+    _id: 0,
+  },
+);
