7c0cdbba1372e19ac4fe427921d62b60ba591fb1
Ederson S <edersonrodrigo31@gmail.com>
2021-08-30 21:46:25 -0300

    fix: requisito 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,2 +1,2 @@
-db.voos.findOne({ "vooId" : 756807 },
-{ "_id": 0, "empresa.sigla" : 1, "empresa.nome": 1, "passageiros": 1 });
+db.voos.findOne({ vooId: 756807 },
+{ _id: 0, "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1 });
