0a6ae78c9b73679f32745127d3f6af297a3b30c5
nyxnil <nsmnilsonn@gmail.com>
2021-09-01 12:50:29 -0300

    desafio 26

--- challenges/desafo26.js
+++ challenges/desafo26.js
@@ -1,1 +1,2 @@
-db.voos.deleteMany({ "empresa.nome": "GOL", "passageiros.pagos": { $gte: 5, $lte: 10 } }); 
+db.voos.deleteMany({ "empresa.nome": "GOL",
+  "passageiros.pagos": { $gte: 5, $lte: 10 } });
