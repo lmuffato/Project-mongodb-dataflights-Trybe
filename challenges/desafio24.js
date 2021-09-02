ecbdf9626def10cd4c3ed6c01b8b97666e15e0d2
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-09-02 12:18:10 -0400

    fix: query 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,5 +1,4 @@
-db.voos
-  .findOne({
-    $nor: [{ litrosCombustivel: 600 }, { "empresa.nome": ["GOL", "AZUL"] }],
-    litrosCombustivel: { $exists: 1 } },
+db.voos.findOne({
+    $nor: [{ "empresa.nome": ["GOL", "AZUL"] }],
+    litrosCombustivel: { $exists: 1, $lt: 600 } },
    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 });
