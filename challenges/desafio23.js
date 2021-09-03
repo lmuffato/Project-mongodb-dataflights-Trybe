b9b5444bfd6a89c50e8ca2963a05bb990dab1fa5
EdmilsonBernardo <ebedmilson48@gmail.com>
2021-09-02 23:41:15 -0300

    feat: requirement 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,13 @@
+db.voos.findOne(
+  {
+    $and: [
+      { litrosCombustivel: { $lte: 1000 } },
+      { litrosCombustivel: { $exists: true } },
+    ],
+  },
+  {
+    vooId: 1,
+    litrosCombustivel: 1,
+    _id: 0,
+  },
+);
