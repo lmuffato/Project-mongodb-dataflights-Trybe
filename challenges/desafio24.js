507ba8407e0e5388c9c7d0824b15a8e26b5aada7
Paulo Henrique <ph.pereira.7@gmail.com>
2021-09-03 08:40:08 -0300

    Desafio 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,5 @@
+db.voos.find(
+    { litrosCombustivel: { $exists: true, $lte: 600 },
+    "empresa.nome": { $nin: ["GOL", "AZUL"] } }, 
+    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
+).limit(1);
