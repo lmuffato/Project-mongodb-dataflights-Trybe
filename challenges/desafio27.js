a9b1134bdf8e5b35a007884b24d8a513c3af4e3d
EdmilsonBernardo <ebedmilson48@gmail.com>
2021-09-03 00:58:31 -0300

    feat: requirement 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,11 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.count(
+    {
+      "empresa.nome": "PASSAREDO",
+      natureza: "Doméstica",
+    },
+  ),
+});
+
+db.resumoVoos.findOne({}, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
