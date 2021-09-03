f8f27f68224d3572f05be58dd6cfdc78c4e17c6c
Paulo Henrique <ph.pereira.7@gmail.com>
2021-09-03 07:54:00 -0300

    Desafio 13:fix, desafio 15

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,1 +1,3 @@
-db.voos.find({ "aeroportoDestino.continente": { $ne: { $in: ["EUROPA", "ÁSIA", "OCEANIA"] } } }).count();
+db.voos.find(
+    { "aeroportoDestino.continente": { $ne: { $in: ["EUROPA", "ÁSIA", "OCEANIA"] } } }
+).count();
